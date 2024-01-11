1440c78589759124d7b3fb6d4ae5151eb7b2a673
Phi Hung Hoang <hphihung795@gmail.com>
2024-01-11 18:14:08 +0700

    Scripts for indexing data and starting typesense server locally

---
+++ scripts/indexTypesense.ts
@@ -0,0 +1,70 @@
+import Typesense from 'typesense';
+import GuitarChordData from './data/guitar.json';
+
+(async () => {
+  const typesense = new Typesense.Client({
+    apiKey: 'xyz',
+    nodes: [
+      {
+        host: 'localhost',
+        port: 8108,
+        protocol: 'http',
+      },
+    ],
+  });
+
+  try {
+    const collection = await typesense.collections('guitar-chords').retrieve();
+    console.log('Found existing collection of guitar-chords');
+
+    if (collection.num_documents !== GuitarChordData.length) {
+      console.log('Collection has different number of documents than data');
+      console.log('Deleting collection');
+      await typesense.collections('guitar-chords').delete();
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log('Creating schema...');
+
+  await typesense.collections().create({
+    name: 'guitar-chords',
+    enable_nested_fields: true,
+    fields: [
+      {
+        name: 'key',
+        type: 'string',
+        facet: true,
+      },
+      {
+        name: 'suffix',
+        type: 'string',
+        facet: true,
+      },
+      {
+        name: 'positions',
+        type: 'object[]',
+      },
+      {
+        name: 'positions.capo',
+        type: 'bool[]',
+        optional: true,
+        facet: true,
+      },
+    ],
+  });
+
+  console.log('Populating collection...');
+
+  try {
+    const returnData = await typesense
+      .collections('guitar-chords')
+      .documents()
+      .import(GuitarChordData);
+
+    console.log('Return data: ', returnData);
+  } catch (err) {
+    console.error(err);
+  }
+})();
